3ada726c639a2370cf03ca8787212052b14c6693
Rick Zweers <rickgz66@gmail.com>
2018-04-09 23:14:15 +0200

    Initial commit

---
+++ app/src/main/java/nl/rickgz/nasa_dscvr_wallpaper/WallpaperToggle.java
@@ -0,0 +1,13 @@
+package nl.rickgz.nasa_dscvr_wallpaper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WallpaperToggle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallpaper_toggle);
+    }
+}
